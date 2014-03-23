fa477b1db5ba7a3080273bc5d91258bb6b25444c
kostya <kx13@ya.ru>
2014-03-23 23:57:48 +0400

    edit

--- clock.c
+++ clock.c
@@ -7,13 +7,8 @@
 // MCU - тип микроконтроллера
 // определены в Makefile
 
-#include <avr/cpufunc.h>
-#include <avr/eeprom.h>
 #include <avr/interrupt.h>
-#include <avr/io.h>
-#include <avr/pgmspace.h>
 #include <inttypes.h>
-#include "util/delay.h"
 
 #include "ds1307.h"
 
@@ -25,14 +20,13 @@
 # error "MAX_TIMERB too large, need increase TIMER_PRESCALERB."
 #endif
 
-#define TIMERA_FREQ 1000UL // Требуемая частота таймера (Герц)
+#define TIMERA_FREQ 300UL // Требуемая частота таймера (Герц)
 #define TIMERA_PRESCALER 256 // Установить значение делителя таймера в TCCR0A (Bits 2:0)
 
 #define MAX_TIMERA  (F_CPU / TIMERA_PRESCALER) / TIMERA_FREQ // максимальное значение таймера в режиме CTC
 #if MAX_TIMERA > UINT8_MAX
 # error "MAX_TIMERA too large, need increase TIMER_PRESCALERA."
 #endif
-
 
 #define low(x)  ((x) & 0xFF)
 #define high(x) (((x) >> 8) & 0xFF)
@@ -43,57 +37,67 @@
 #define FIRST_DIGIT 2
 #define LAST_DIGIT  5
 
-#define ALL_KEYS_UP 3
-#define HOUR_KEY 1
-#define MINUTE_KEY 2
 
 const  int8_t led_digits[] = {64, 121, 36, 48, 25, 18, 2, 120, 0, 16};
 
 uint8_t time[4];
 
-volatile uint8_t read_time = 1;
-volatile uint8_t key_pressed = 0;
-volatile uint8_t oldkeys = ALL_KEYS_UP;
+volatile uint8_t get_time = 1;
+volatile uint8_t set_time = 0;
 
-volatile uint8_t current_digit = FIRST_DIGIT;
+volatile uint8_t delay = 0;
 
-inline void show_clock(void)
+static inline void show_clock(void)
 {
+  static  uint8_t current_digit = FIRST_DIGIT;
+
 //погасить текущий
-reset_bit(PORTD, current_digit);
+  reset_bit(PORTD, current_digit);
 
-current_digit++;
-if(current_digit > LAST_DIGIT)
-current_digit = FIRST_DIGIT;
+  current_digit++;
+  if(current_digit > LAST_DIGIT)
+    current_digit = FIRST_DIGIT;
 
-PORTB = led_digits[time[current_digit - FIRST_DIGIT]];
+  PORTB = led_digits[time[current_digit - FIRST_DIGIT]];
 
 //зажечь следующий индикатор
-set_bit(PORTD, current_digit);
+  set_bit(PORTD, current_digit);
 }
 
 inline void is_key_pressed(void)
 {
-uint8_t keys = PIND & 0x03;
-uint8_t pressed = 0;
-if(key_pressed)
-return;
+  const uint8_t KEY_DELAY = 100;
+  const uint8_t  ALL_KEYS = 3;
+  const uint8_t  HOUR_KEY = 1;
+  const uint8_t  MINUTE_KEY = 2;
+
+  if(set_time)
+    return;
+
+  uint8_t keys = PIND & ALL_KEYS;
+  
+  if(keys < ALL_KEYS && delay < KEY_DELAY)
+    delay++;
+  else  
+    delay = 0;
+ 
+  if(delay < KEY_DELAY)
+    return;
 
 //установка часов
-if (!(keys & HOUR_KEY) && (oldkeys & HOUR_KEY))
-{
-time[1]++;
-if ((time[1]>9 && time[0]<2) || (time[1]>3 && time[0]==2 )  )
-{
-time[1] = 0;
-time[0]++;
-}
-if (time[0]>2)
-time[0]=0;
-    pressed = 1;
+  if (!(keys & HOUR_KEY) )
+  {
+    time[1]++;
+    if ((time[1]>9 && time[0]<2) || (time[1]>3 && time[0]==2 )  )
+    {
+      time[1] = 0;
+      time[0]++;
+    }
+    if (time[0]>2)
+      time[0]=0;
   }
   //установка минут
-  else if ( !(keys & MINUTE_KEY) && (oldkeys & MINUTE_KEY))
+  else if ( !(keys & MINUTE_KEY))
   {
     time[3]++;
     if (time[3]>9)
@@ -104,20 +108,17 @@ time[0]=0;
 
     if (time[2]>5)
       time[2]=0;
-
-    pressed = 1;    
   }
-  oldkeys = keys;
-  key_pressed =  pressed;    
+//      ds1307_settime(time);
+
+  set_time =  1;    
 }
 
-//динамическая индикация
 ISR (TIMER0_COMPA_vect)
 {
   show_clock();
 }
 
-//опрос клавиатуры
 ISR (TIMER0_COMPB_vect)
 {
   is_key_pressed();
@@ -126,7 +127,9 @@ ISR (TIMER0_COMPB_vect)
 // отсчет интервалов считывания показаний
 ISR (TIMER1_COMPA_vect)
 {
-  read_time = 1;
+  get_time = 1;
+  //      ds1307_gettime(time);
+
 }
 
 int main(void)
@@ -157,16 +160,16 @@ int main(void)
 
   for(;;)
   {
-    if(read_time)
+    if(get_time)
     {
       ds1307_gettime(time);
-      read_time = 0;
+      get_time = 0;
     }
 
-    if(key_pressed)
+    if(set_time)
     {
       ds1307_settime(time);
-      key_pressed = 0;
+      set_time = 0;
     }
   }
 }
